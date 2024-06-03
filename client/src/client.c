fa6d6e2ef109ef253fc8b9095ecf5102c1291d5b
Erick Vazquez <erickescorpions@hotmail.com>
2024-06-03 14:23:29 -0600

    formateo y limpieza de archivo

--- client/src/client.c
+++ client/src/client.c
@@ -14,11 +14,13 @@
 #define MAXDATASIZE 100
 #define MAXDATASIZE_RESP 20000
 
-char* read_command();
+char *read_command();
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-  if (argc != 3) {
+  if (argc != 3)
+  {
     fprintf(stderr, "Uso del programa: %s <hostname> <puerto>\n", argv[0]);
     exit(1);
   }
@@ -27,37 +29,41 @@ int main(int argc, char *argv[]) {
   int numbytes;
   char buf[MAXDATASIZE_RESP];
 
-  int sockfd;  
+  int sockfd;
   struct hostent *he;
-  // informacion de la direccion de destino 
+  // informacion de la direccion de destino
   struct sockaddr_in conexion_servidor;
 
-  // obtener informacion de host servidor 
-  if ((he=gethostbyname(argv[1])) == NULL) { 
-   perror("gethostbyname");
-   exit(1);
+  // obtener informacion de host servidor
+  if ((he = gethostbyname(argv[1])) == NULL)
+  {
+    perror("gethostbyname");
+    exit(1);
   }
 
   // creamos el socket
-  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-   perror("socket");
-   exit(1);
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+  {
+    perror("socket");
+    exit(1);
   }
 
-  conexion_servidor.sin_family = AF_INET; 
-  conexion_servidor.sin_port = htons( atoi(argv[2]) ); 
+  conexion_servidor.sin_family = AF_INET;
+  conexion_servidor.sin_port = htons(atoi(argv[2]));
   conexion_servidor.sin_addr = *((struct in_addr *)he->h_addr);
-  memset(&(conexion_servidor.sin_zero), '\0',8);  // poner a cero el resto de la estructura 
+  memset(&(conexion_servidor.sin_zero), '\0', 8); // poner a cero el resto de la estructura
 
-  if (connect(sockfd, (struct sockaddr *)&conexion_servidor, sizeof(struct sockaddr)) == -1) {
+  if (connect(sockfd, (struct sockaddr *)&conexion_servidor, sizeof(struct sockaddr)) == -1)
+  {
     perror("connect");
     exit(1);
   }
-  
-  char* comando = NULL;
 
-  do {
-    // Limpiamos el buffer
+  char *comando = NULL;
+
+  do
+  {
+    // limpiamos el buffer
     memset(buf, 0, sizeof(buf));
 
     fputs("Servidor $ ", stdout);
@@ -66,22 +72,24 @@ int main(int argc, char *argv[]) {
     size_t len_comando = strlen(comando) - 1;
 
     // Se envia el comando al servidor
-    if(send(sockfd, comando, len_comando, 0) == -1) {
+    if (send(sockfd, comando, len_comando, 0) == -1)
+    {
       perror("send()");
       exit(1);
     }
 
-    
-    if ((numbytes = recv(sockfd, buf, MAXDATASIZE_RESP-1, 0)) == -1) {
+    if ((numbytes = recv(sockfd, buf, MAXDATASIZE_RESP - 1, 0)) == -1)
+    {
       perror("recv");
       exit(1);
     }
 
     // Si el mensaje recibido es OK, el comando no tiene salida
-    if(strcmp(buf, "OK") != 0) {
-      printf("\n%s\n",buf);
+    if (strcmp(buf, "OK") != 0)
+    {
+      printf("\n%s\n", buf);
     }
-  } while(strcmp(comando, "exit\n") != 0);
+  } while (strcmp(comando, "exit\n") != 0);
 
   free(comando);
   close(sockfd);
@@ -89,11 +97,13 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-char* read_command() {
+char *read_command()
+{
   char *input = NULL;
   size_t buffer_size = 0;
 
-  if (getline(&input, &buffer_size, stdin) == -1){
+  if (getline(&input, &buffer_size, stdin) == -1)
+  {
     perror("read command");
     exit(1);
   }
